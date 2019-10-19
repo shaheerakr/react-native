e3e51af0e1d246bbc088f358e15002ae3564a482
Shaheer Akram <shaheerakram@icloud.com>
2019-10-19 21:56:21 +0500

    forms

---
+++ forms/App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TextInput
+} from 'react-native';
+
+export default class App extends Component{
+  render(){
+    return (
+      <View>
+        <TextInput/>
+      </View>
+    );
+ }
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+//export default App;
