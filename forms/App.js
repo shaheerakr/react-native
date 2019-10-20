d54f0af90d5ef58175dad0f3c7499c191a80fd87
Shaheer Akram <shaheerakram@icloud.com>
2019-10-21 03:14:31 +0500

    login screen

--- forms/App.js
+++ forms/App.js
@@ -14,20 +14,71 @@ import {
   View,
   Text,
   StatusBar,
-  TextInput
+  TextInput,
+  Button
 } from 'react-native';
 
 export default class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      username : "",
+      password : ""
+    }
+  }
+
+  handleChangeUsername = (newText) =>{
+    this.setState({
+      username : newText
+    })
+  }
+  handleChangePassword = (newText) =>{
+    this.setState({
+      password : newText
+    })
+  }
+  handleSubmit = () =>{
+    this.setState({
+      username : "",
+      password : ""
+    })
+    let textInput = this.refs["textInput"]
+    textInput.clear()
+  }
+  
   render(){
     return (
-      <View>
-        <TextInput/>
+      <View style = {styles.container}>
+        <View>
+          <Text> Username: </Text>
+          <TextInput
+            defaultValue = {this.state.username}
+            onChangeText = {this.handleChangeUsername}
+            ref = "textInput"
+          />
+        </View>
+        <View>
+          <Text> Password: </Text>
+          <TextInput
+            defaultValue = {this.state.password}
+            onChangeText = {this.handleChangePassword}
+            ref = "textInput"
+          />
+        </View>
+        <Button
+          onPress = {this.handleSubmit}
+          title = "Sign in"
+        />
       </View>
     );
  }
 };
 
 const styles = StyleSheet.create({
+  container :{
+    flex : 1,
+    justifyContent: 'center',
+  },
   
 });
 
